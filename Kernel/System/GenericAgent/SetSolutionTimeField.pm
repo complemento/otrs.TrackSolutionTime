4a1b986ad991cab116de4e16ca06eea7b4779459
Ricardo Silva <ricardo.silva@complemento.net.br>
2018-05-15 08:25:32 -0300

    Release 1 - added code comments

--- Kernel/System/GenericAgent/SetSolutionTimeField.pm
+++ Kernel/System/GenericAgent/SetSolutionTimeField.pm
@@ -33,13 +33,12 @@ sub new {
 sub Run {
     my ( $Self, %Param ) = @_;
     
+    #Create system objects that will be used above
     my $DynamicFieldObject = $Kernel::OM->Get('Kernel::System::DynamicField');
     my $UserObject = $Kernel::OM->Get('Kernel::System::User');
     my $DynamicFieldBackendObject = $Kernel::OM->Get('Kernel::System::DynamicField::Backend');
     my $DynamicFieldValueObject = $Kernel::OM->Get('Kernel::System::DynamicFieldValue');
     my $TimeObject = $Kernel::OM->Get('Kernel::System::Time');
-    my $Overwrite = "Yes";    
-
     my $TicketObject = $Kernel::OM->Get('Kernel::System::Ticket');
 
     # get ticket data
@@ -49,35 +48,54 @@ sub Run {
         Extended => 1,
     );
 
+    #Get Dynamic Field Configuration to SolutionTime
+    #this dynamic field is used to receive the total time
+    #spend to resolve a ticket or the total time passed from
+    #the maximum time permited to do that
     my $DynamicField = $DynamicFieldObject->DynamicFieldGet(
        Name => 'SolutionTime'
     );
 
+    #Get Dynamic Field Configuration to IsSolutionTimeSLAStoppedCalculated
+    #This dynamic field was created to control the calculation when the
+    #ticket is SLA Paused
     my $DynamicFieldIsCalc = $DynamicFieldObject->DynamicFieldGet(
        Name => 'IsSolutionTimeSLAStoppedCalculated'
     );
 
+    #Get Dynamic Field Configuration to TotalTime
+    #this dynamic field is used to receive the total time worked on the
+    #ticket in minutes of work time.
     my $DynamicFieldTotalTime = $DynamicFieldObject->DynamicFieldGet(
        Name => 'TotalTime'
     );
 
+    #Get Dynamic Field Configuration to PercentualScaleSLA
+    #this dynamic field is a Dropbox field. It contains severals options of
+    #percentual range of total time to resolve a ticket in relation of expected
+    #time to do this defined into the SLA configuration.
     my $DynamicFieldPercentualScaleSLA = $DynamicFieldObject->DynamicFieldGet(
 		Name => "PercentualScaleSLA",
 	);
 
+    #This generic agent will be executed when the ticket has a SLA configurated
 	if(defined $Ticket{SLA}){
 		
-		#Preenchimento de Solution Time
+		#Verify if the ticket is closed
         if($Ticket{StateType} eq 'closed'){
+            #Get the time that the ticket was paused by SLA
             my $PendSumTime = $TicketObject->GetTotalNonEscalationRelevantBusinessTime(
                 TicketID => $Ticket{TicketID},
             )||0;
 
+            #Get the data of Ticket Escalation 
             my %Escalation = $TicketObject->TicketEscalationPreferences(
                 Ticket => \%Ticket,
                 UserID => 1,
             );
 
+            #When the ticket is closed the SolutionTime value is got from
+            #SolutionDiffInMin Extend Field from Ticket object.
             my $Success = $DynamicFieldValueObject->ValueSet(
                 FieldID  => $DynamicField->{ID},
                 ObjectID => $Ticket{TicketID},
@@ -89,6 +107,8 @@ sub Run {
                 UserID   => 1,
             );
 
+            #When the ticket is closed the TotalTime value is got from
+            #SolutionInMin Extend Field from Ticket object subtracting the ticket stopped time
             $Success = $DynamicFieldValueObject->ValueSet(
                 FieldID  => $DynamicFieldTotalTime->{ID},
                 ObjectID => $Ticket{TicketID},
@@ -100,11 +120,15 @@ sub Run {
                 UserID   => 1,
             );
 
+            #Calcule the percentual of time spended to resolve the ticket in relationship
+            #with the time defined in SLA configuration
             my $percent = 0;
             if($Escalation{SolutionTime} != 0){
                 $percent = ($Ticket{SolutionInMin} * 100) / $Escalation{SolutionTime};
             }
 
+            #Select the combo box PercentualScaleSLA value in relationship with
+            #percentual
             $Success = $DynamicFieldValueObject->ValueSet(
                 FieldID  => $DynamicFieldPercentualScaleSLA->{ID},
                 ObjectID => $Ticket{TicketID},
@@ -117,15 +141,20 @@ sub Run {
             );
         }
         else{
+            #If the ticket is open yet verify if it is in SLA stop
             my $notIsSLAStopped = ($TimeObject->TimeStamp2SystemTime(
                 String => $Ticket{SolutionTimeDestinationDate},
             ) != 1767139200);
 
+            #Get the time that the ticket was paused by SLA
             my $PendSumTime = $TicketObject->GetTotalNonEscalationRelevantBusinessTime(
                 TicketID => $Ticket{TicketID},
             )||0;
 
+            #Rules for no SLA Stopped Tickets
             if($notIsSLAStopped){
+                #When the ticket is open and no SLA Stopped get the solution time from
+                #SolutionTimeWorkingTime Field
                 my $Success = $DynamicFieldValueObject->ValueSet(
                     FieldID  => $DynamicField->{ID},
                     ObjectID => $Ticket{TicketID},
@@ -137,11 +166,15 @@ sub Run {
                     UserID   => 1,
                 );
 
+                #Get the data of Ticket Escalation 
                 my %Escalation = $TicketObject->TicketEscalationPreferences(
                     Ticket => \%Ticket,
                     UserID => 1,
                 );
 
+                #When the ticket is open and no SLA Stopped the Total WorkingTime 
+                #is calculated with the Ticket Creation Date and the Actual Date
+                #subtracting the Stopped Time.
                 my $WorkingTime = $TimeObject->WorkingTime(
                     StartTime => $TimeObject->TimeStamp2SystemTime(
                         String => $Ticket{Created},
@@ -161,11 +194,15 @@ sub Run {
                     UserID   => 1,
                 );
 
+                #Calcule the percentual of time spended to resolve the ticket in relationship
+                #with the time defined in SLA configuration
                 my $percent = 0;
                 if($Escalation{SolutionTime} != 0){
                     $percent = (($WorkingTime/60) * 100) / $Escalation{SolutionTime};
                 }
 
+                #Select the combo box PercentualScaleSLA value in relationship with
+                #percentual
                 $Success = $DynamicFieldValueObject->ValueSet(
                     FieldID  => $DynamicFieldPercentualScaleSLA->{ID},
                     ObjectID => $Ticket{TicketID},
@@ -177,6 +214,8 @@ sub Run {
                     UserID   => 1,
                 );
 
+                #When the ticket is oppened and no SLA Stopped, update the IsSolutionTimeSLAStoppedCalculated
+                #dynamic field to false (0)
                 $DynamicFieldValueObject->ValueSet(
                     FieldID  => $DynamicFieldIsCalc->{ID},
                     ObjectID => $Ticket{TicketID},
@@ -189,6 +228,8 @@ sub Run {
                 );
             }
             else{
+                #Whe the ticket is open and SLA Stopped verify IsSolutionTimeSLAStoppedCalculated
+                #dynamic field
                 my $ValueIsCalc = $DynamicFieldValueObject->ValueGet(
                     FieldID            => $DynamicFieldIsCalc->{ID},
                     ObjectID           => $Ticket{TicketID},               
@@ -200,11 +241,18 @@ sub Run {
                     $isCalculated = $ValueIsCalc->[0]->{ValueInt};
                 }
 
+                #if IsSolutionTimeSLAStoppedCalculated dynamic field is false (0)
+                #make the calculations
                 if($isCalculated == 0){
+
+                    #Get the data of Ticket Escalation 
                     my %Escalation = $TicketObject->TicketEscalationPreferences(
                         Ticket => \%Ticket,
                         UserID => 1,
                     );
+
+                    #When the ticket is SLA Stopped is necessary to calculate the last
+                    #destination time value to make the next values
                     my $DestinationTime = $TimeObject->DestinationTime(
                         StartTime => $TimeObject->TimeStamp2SystemTime(
                             String => $Ticket{Created},
@@ -215,6 +263,9 @@ sub Run {
                         # 
                         Calendar => $Escalation{Calendar},
                     );
+
+                    #if the last destination time valid is bigger than the actual date
+                    #the solution time is from actual date to last destination date
                     my $SolutionTime = 0;
                     if($TimeObject->SystemTime() < $DestinationTime){
                         $SolutionTime = $TimeObject->WorkingTime(
@@ -224,6 +275,8 @@ sub Run {
                         );
                     }
                     else{
+                        #if the last destination time valid is smaller than the actual date
+                        #the solution time is from last destination date to actual date
                         $SolutionTime = $TimeObject->WorkingTime(
                             StartTime => $DestinationTime,
                             StopTime  => $TimeObject->SystemTime(),
@@ -242,6 +295,9 @@ sub Run {
                         UserID   => 1,
                     );
 
+                    #When the ticket is SLA Stopped the Total WorkingTime 
+                    #is calculated with the Ticket Creation Date and the Actual Date
+                    #subtracting the Stopped Time.
                     my $WorkingTime = $TimeObject->WorkingTime(
                         StartTime => $TimeObject->TimeStamp2SystemTime(
                             String => $Ticket{Created},
@@ -261,11 +317,15 @@ sub Run {
                         UserID   => 1,
                     );
 
+                    #Calcule the percentual of time spended to resolve the ticket in relationship
+                    #with the time defined in SLA configuration
                     my $percent = 0;
                     if($Escalation{SolutionTime} != 0){
                         $percent = (($WorkingTime/60) * 100) / $Escalation{SolutionTime};
                     }
 
+                    #Select the combo box PercentualScaleSLA value in relationship with
+                    #percentual
                     $Success = $DynamicFieldValueObject->ValueSet(
                         FieldID  => $DynamicFieldPercentualScaleSLA->{ID},
                         ObjectID => $Ticket{TicketID},
@@ -277,6 +337,9 @@ sub Run {
                         UserID   => 1,
                     );
 
+                    #When the ticket is SLA Stopped, update the IsSolutionTimeSLAStoppedCalculated
+                    #dynamic field to true (1). This ensures that this calcule will be made just one
+                    #time while this ticket is stopped 
                     $DynamicFieldValueObject->ValueSet(
                         FieldID  => $DynamicFieldIsCalc->{ID},
                         ObjectID => $Ticket{TicketID},
@@ -294,6 +357,13 @@ sub Run {
 	
 }
 
+#This function convert one percentual to any scale configured in the dynamic field PercentualScaleSLA
+#The rules are: 
+#   * if the option have "-" signal the system will consider that the value have an init and an end and
+#the value will be major or equal and minor.
+#   * if the option have ">" signal the system will consider that the value have to be major of it;
+#   * if the option have "<" signal the system will consider that the value have to be minor of it;
+#   * if none of values will be found the function will return empty string;
 sub ConvertPercentualSLAScale{
     my ( $Self, %Param ) = @_;
 
